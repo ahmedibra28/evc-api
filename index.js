4d187bb7512ab052b5a28b53f764bb0fc658d914
Ahmed <ahmaat19@gmail.com>
2023-05-13 09:17:08 +0300

    updating packages

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
 const axios = require('axios')
 const { v4: uuidv4 } = require('uuid')
 
-// get EVC from customer/buyer
 async function evc(props) {
   try {
     if (!props) return 'you have to provide an object props'
 
-    const {
-      merchant_u_id,
-      api_user_id,
-      api_key,
-      client_mobile_number,
+    let {
+      merchantUId,
+      apiUserId,
+      apiKey,
+      customerMobileNumber,
       description,
       amount,
-      merchant_no,
-      auto_withdraw,
+      autoWithdraw,
+      merchantNo,
     } = props
 
-    const responseMsg = (responseCode, responseMsg) => ({
-      responseCode,
-      responseMsg,
+    const responseMsg = (status, message) => ({
+      status,
+      message,
     })
 
-    if (!merchant_u_id) return responseMsg(400, 'merchant_u_id is required')
-    if (!api_user_id) return responseMsg(400, 'api_user_id is required')
-    if (!api_key) return responseMsg(400, 'api_key is required')
-    if (!client_mobile_number)
-      return responseMsg(400, 'client_mobile_number is required')
-    if (!description) return responseMsg(400, 'description is required')
-    if (!amount) return responseMsg(400, 'amount is required')
-    if (client_mobile_number.toString().length !== 9)
-      return responseMsg(400, 'client_mobile_number must be 9 digits')
+    if (!merchantUId) return responseMsg(400, 'Merchant user id is required')
+    if (!apiUserId) return responseMsg(400, 'API user id is required')
+    if (!apiKey) return responseMsg(400, 'API key is required')
+    if (!customerMobileNumber)
+      return responseMsg(400, 'Customer mobile number is required')
+    if (!description) return responseMsg(400, 'Description is required')
+    if (!amount) return responseMsg(400, 'Amount is required')
+    if (customerMobileNumber.toString().length !== 12)
+      return responseMsg(400, 'Customer mobile number must be 12 digits')
 
-    if (auto_withdraw) {
-      if (merchant_no.toString().length !== 9)
-        return responseMsg(400, 'merchant_no must be 9 digits')
+    if (autoWithdraw) {
+      if (merchantNo?.toString().length !== 9)
+        return responseMsg(400, 'Merchant number must be 9 digits')
     }
 
     const paymentObject = {
@@ -44,16 +43,16 @@ async function evc(props) {
       channelName: 'WEB',
       serviceName: 'API_PURCHASE',
       serviceParams: {
-        merchantUid: merchant_u_id,
-        apiUserId: api_user_id,
-        apiKey: api_key,
-        paymentMethod: 'mwallet_account',
+        merchantUid: merchantUId,
+        apiUserId: apiUserId,
+        apiKey: apiKey,
+        paymentMethod: 'MWALLET_ACCOUNT',
         payerInfo: {
-          accountNo: `252${client_mobile_number}`,
+          accountNo: customerMobileNumber,
         },
         transactionInfo: {
           referenceId: uuidv4(),
-          invoiceId: `${uuidv4().slice(0, 5)}-${client_mobile_number}`,
+          invoiceId: `${uuidv4().slice(0, 5)}-${customerMobileNumber}`,
           amount: Number(amount),
           currency: 'USD',
           description: description,
@@ -70,17 +69,17 @@ async function evc(props) {
       channelName: 'WEB',
       serviceName: 'API_CREDITACCOUNT',
       serviceParams: {
-        merchantUid: merchant_u_id,
-        apiUserId: api_user_id,
-        apiKey: api_key,
+        merchantUid: merchantUId,
+        apiUserId: apiUserId,
+        apiKey: apiKey,
         paymentMethod: 'MWALLET_ACCOUNT',
         payerInfo: {
-          accountNo: merchant_no,
+          accountNo: merchantNo,
           accountType: 'MERCHANT',
         },
         transactionInfo: {
           referenceId: uuidv4(),
-          invoiceId: `${uuidv4().slice(0, 5)}-${merchant_no}`,
+          invoiceId: `${uuidv4().slice(0, 5)}-${merchantNo}`,
           amount: Number(amount) - netAmount,
           currency: 'USD',
           description: description,
@@ -89,20 +88,20 @@ async function evc(props) {
     }
 
     const { data } = await axios.post(
-      `https://api.waafi.com/asm`,
+      `https://api.waafipay.net/asm`,
       paymentObject
     )
 
     // 5206 => payment has been cancelled
     // 2001 => payment has been done successfully
     if (Number(data.responseCode) === 2001) {
-      if (auto_withdraw) {
-        await axios.post(`https://api.waafi.com/asm`, withdrawalObject)
+      if (autoWithdraw) {
+        await axios.post(`https://api.waafipay.net/asm`, withdrawalObject)
       }
     }
     return await data
-  } catch (error) {
-    return error
+  } catch ({ message }) {
+    return message
   }
 }
 
